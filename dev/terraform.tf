d2148d9a8406e142767299696adbdf882485624b
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-24 11:29:44 +0530

    All Env-In Single Push - Try 001

---
+++ dev/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization = "tforg100"
+    workspaces {
+        name = "gittrig_execrm-ws100-dv"
+    }
+  }
+}
